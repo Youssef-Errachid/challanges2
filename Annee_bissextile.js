3b5b7089ded1e27a2980597a058c004e050521b2
Youssef-Errachid <yousseferrachid91@gmail.com>
2025-09-24 22:44:59 +0200

    Update Annee_bissextile.js

--- Annee_bissextile.js
+++ Annee_bissextile.js
@@ -1,5 +1,11 @@
 const prompt = require('prompt-sync')();
 
-let annee = prompt("please ente the anne.");
+let annee = prompt("please ente the year.");
 
-if((annee % 4 === 0 && annee % 100 !== 0) || (annee % 400 === 0 ));
+if((annee % 4 === 0 && annee % 100 !== 0) || (annee % 400 === 0 ));{
+  console.log(annee , "est une annee bissextile");
+}
+else{
+  console.log(annee , " n'est pas une annee bissextile");
+}
+  
